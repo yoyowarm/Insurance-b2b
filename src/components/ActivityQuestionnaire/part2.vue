b4a70377a827cefd6ac9949a96e4e98e872fe7a8
evan <evan@goodarc.com>
2022-09-21 14:03:16 +0800

    fix: 🐛 修改 radio 重複 id

--- src/components/ActivityQuestionnaire/part2.vue
+++ src/components/ActivityQuestionnaire/part2.vue
@@ -2,8 +2,8 @@
   <div class="w-full my-4 dashed-border">
     <div class="w-full flex flex-row mt-4" v-for="(item,index) in questionList" :key="item">
       <div class=" flex flex-row justify-between">
-        <RadioInput text="是" :id="`${questionListID[index]}${index}`" :value="data.sheet1.part2[questionListID[index]] === true" @updateValue="updateValue(true, questionListID[index])"/>
-        <RadioInput class="mx-2" text="否" :id="`${questionListID[index]}${index}2`" :value="data.sheet1.part2[questionListID[index]] === false" @updateValue="updateValue(false, questionListID[index])"/>
+        <RadioInput text="是" :id="`${questionListID[index]}${index}p`" :value="data.sheet1.part2[questionListID[index]] === true" @updateValue="updateValue(true, questionListID[index])"/>
+        <RadioInput class="mx-2" text="否" :id="`${questionListID[index]}${index}2p`" :value="data.sheet1.part2[questionListID[index]] === false" @updateValue="updateValue(false, questionListID[index])"/>
     </div>
       <div class=" text-lg">{{item}}</div>
     </div>
