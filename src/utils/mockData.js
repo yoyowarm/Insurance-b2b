c56035dbd2f3a8d05780ad9104dbf7811c672b0b
evan <evan@goodarc.com>
2022-12-13 17:27:56 +0800

    fix: 🐛修改報價明細寶格欄位寬度

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -179,12 +179,12 @@ export const quotationListTable = () => {
       {
         text: '要保人',
         value: 'applicantName',
-        size: '3-6'
+        size: '2-6'
       },
       {
         text: '被保人',
         value: 'insuredName',
-        size: '3-6'
+        size: '2-6'
       },
       {
         text: '保費',
@@ -324,12 +324,12 @@ export const auditListTable = () => {
       {
         text: '要保人',
         value: 'applicantName',
-        size: '3-6'
+        size: '2-6'
       },
       {
         text: '被保人',
         value: 'insuredName',
-        size: '3-6'
+        size: '2-6'
       },
       {
         text: '保費',
