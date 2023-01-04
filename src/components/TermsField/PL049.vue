52219ad8e2e0a4b48abad8497107ab39dc9225a0
evan <evan@goodarc.com>
2023-01-04 10:50:39 +0800

    fix:🐛修改條款必填

--- src/components/TermsField/PL049.vue
+++ src/components/TermsField/PL049.vue
@@ -2,7 +2,7 @@
 	<div>
 		<FormTitle title="承租人借用人責任附加條款(保額外加)" classList="text-xl text-gray-700">
       <font-awesome-icon class="text-xl text-gray-700 mr-1" :icon="['far', 'clipboard']" slot="left"/>
-      <span class="text-main ml-2" slot="right">*必填</span>
+      <span class="text-main ml-2 whitespace-no-wrap" slot="right">*必填</span>
     </FormTitle>	
     <FormTitle title="本附加條款之每一意外事故及保險期間累計保險金額最高以新台幣" classList="text-lg text-gray-700 mt-3"/>
      <div class="column-5 ">
--- src/components/TermsField/PL053.vue
+++ src/components/TermsField/PL053.vue
@@ -2,7 +2,7 @@
 	<div>
 		<FormTitle title="傷害醫療及身故慰問金費用附加條款" classList="text-xl text-gray-700">
       <font-awesome-icon class="text-xl text-gray-700 mr-1" :icon="['far', 'clipboard']" slot="left"/>
-      <span class="text-main ml-2" slot="right">*必填</span>
+      <span class="text-main ml-2 whitespace-no-wrap" slot="right">*必填</span>
     </FormTitle>	
     <FormTitle title="每一個人傷害醫療慰問金費用保險金額" classList="text-lg text-gray-700 mt-3"/>
      <div class="column-5 mt-2">
