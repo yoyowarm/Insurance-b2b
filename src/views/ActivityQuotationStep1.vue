7080f946bf0130da14045acaa7cff672853cc194
evan <evan@goodarc.com>
2023-01-10 12:14:01 +0800

    fix:🐛審核時修改填寫詢問表>查看詢問表

--- src/views/ActivityQuotationStep1.vue
+++ src/views/ActivityQuotationStep1.vue
@@ -80,7 +80,7 @@
       <div class="flex flex-col justify-center items-center sm:flex-row">
         <Button @click.native="calculateAmount" :disabled="calculateModel" class="my-2 sm:my-6 w-56 md:w-32 sm:mr-4" outline>試算</Button>
         <Button @click.native="correctAmount" :disabled="!calculateModel" class="my-2 sm:my-6 w-56 md:w-32 sm:mr-4" outline>更正</Button>
-        <Button :disabled="calculateModel  && InsuranceActive !== 7" @click.native="() => { if(!calculateModel || InsuranceActive == 7) {openQuestionnaire = true}}" class="my-2 sm:my-6 w-56 md:w-56" outline>填寫詢問表({{ underwriteCoefficient }})</Button>
+        <Button :disabled="calculateModel  && InsuranceActive !== 7" @click.native="() => { if(!calculateModel || InsuranceActive == 7) {openQuestionnaire = true}}" class="my-2 sm:my-6 w-56 md:w-56" outline>{{InsuranceActive == 7?'查看詢問表':'填寫詢問表'}}({{ underwriteCoefficient }})</Button>
       </div>
       <div class="flex flex-col justify-center items-center sm:flex-row">
         <Button @click.native="nextStep" class="my-4 w-56 md:w-42" :class="{'md:mr-5': underwriteStatus.underwriteDirection == 1}">下一步</Button>
