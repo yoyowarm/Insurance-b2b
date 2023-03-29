1c9d071f228f7c896f2ac342992d59f9de7005f3
evan <evan@goodarc.com>
2023-03-29 17:01:55 +0800

    fix:

--- src/router/index.js
+++ src/router/index.js
@@ -8,216 +8,216 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    component: () => import(/* webpackChunkName: "home" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       {
         path: '/',
         name: 'home',
-        component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue'),
+        component: () => import('../views/HomeView.vue'),
       }
     ]
   },
   {
     path: '/news',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //最新消息
         path: '/news/list',
         name: 'newsList',
-        component: () => import(/* webpackChunkName: "about" */ '../views/NewsListView.vue')
+        component: () => import('../views/NewsListView.vue')
       }
     ]
   },
   {
     path: '/product',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //商品資訊
         path: '/product/list',
         name: 'productList',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ProductListView.vue')
+        component: () => import('../views/ProductListView.vue')
       }
     ]
   },
   {
     path: '/quotation-list',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //報價明細
         path: '/quotation-list',
         name: 'quotationList',
-        component: () => import(/* webpackChunkName: "about" */ '../views/QuotationList.vue')
+        component: () => import('../views/QuotationList.vue')
       },
     ]
   },
   {
     path: '/underwriting-list',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //核保明細
         path: '/underwriting-list',
         name: 'underwritingList',
-        component: () => import(/* webpackChunkName: "about" */ '../views/QuotationList.vue')
+        component: () => import('../views/QuotationList.vue')
       },
     ]
   },
   {
     path: '/place-quotation',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { // 場所報價第一步
         path: '/place-quotation/step1',
         name: 'place-quotation-step1',
-        component: () => import(/* webpackChunkName: "about" */ '../views/PlaceQuotationStep1.vue')
+        component: () => import('../views/PlaceQuotationStep1.vue')
       },
       { // 場所報價第二步
         path: '/place-quotation/step2',
         name: 'place-quotation-step2',
-        component: () => import(/* webpackChunkName: "about" */ '../views/PlaceQuotationStep2.vue')
+        component: () => import('../views/PlaceQuotationStep2.vue')
       },
       { // 場所報價第三步
         path: '/place-quotation/step3',
         name: 'place-quotation-step3',
-        component: () => import(/* webpackChunkName: "about" */ '../views/PlaceQuotationStep3.vue')
+        component: () => import('../views/PlaceQuotationStep3.vue')
       },
     ]
   },
   {
     path: '/activity-quotation',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { // 活動報價第一步
         path: '/activity-quotation/step1',
         name: 'activity-quotation-step1',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ActivityQuotationStep1.vue')
+        component: () => import('../views/ActivityQuotationStep1.vue')
       },
       { // 活動報價第二步
         path: '/activity-quotation/step2',
         name: 'activity-quotation-step2',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ActivityQuotationStep2.vue')
+        component: () => import('../views/ActivityQuotationStep2.vue')
       },
       { // 活動報價第三步
         path: '/activity-quotation/step3',
         name: 'activity-quotation-step3',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ActivityQuotationStep3.vue')
+        component: () => import('../views/ActivityQuotationStep3.vue')
       },
     ]
   },
   {
     path: '/questionnaire-management',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { // 詢問表管理
         path: '/questionnaire-management',
         name: 'questionnaire-management',
-        component: () => import(/* webpackChunkName: "about" */ '../views/QuestionnaireManagement.vue')
+        component: () => import('../views/QuestionnaireManagement.vue')
       }
     ]
   },
   {
     path: '/Office-audit',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { // 總公司核保
         path: '/Office-audit',
         name: 'Office-audit',
-        component: () => import(/* webpackChunkName: "about" */ '../views/OfficeAudit.vue')
+        component: () => import('../views/OfficeAudit.vue')
       },
     ]
   },
   {
     path: '/branch-audit',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //分公司核保
         path: '/branch-audit',
         name: 'branch-audit',
-        component: () => import(/* webpackChunkName: "about" */ '../views/BranchAudit.vue')
+        component: () => import('../views/BranchAudit.vue')
       }
     ]
 
   },
   {
     path: '/permissionSetting',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //權限設定
         path: '/',
         name: 'permissionSetting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/PermissionSetting.vue')
+        component: () => import('../views/PermissionSetting.vue')
       }
     ]
   },
   {
     path: '/parameterSetting',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //參數設定 類別
         path: '/parameterSetting/category',
         name: 'parameterSetting-category',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/Category.vue')
+        component: () => import('../views/ParameterSetting/Category.vue')
       },
       { //參數設定 縣市最低保額
         path: '/parameterSetting/minimumAmount',
         name: 'parameterSetting-minimumAmount',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/MinimumAmount.vue')
+        component: () => import('../views/ParameterSetting/MinimumAmount.vue')
       },
       {
         path: '/parameterSetting/basic-premium-setting',
         name: 'parameterSetting-basicPremiumSetting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/BasicPremiumSetting.vue')
+        component: () => import('../views/ParameterSetting/BasicPremiumSetting.vue')
       },
       {
         path: '/parameterSetting/quote-separately',
         name: 'parameterSetting-quoteSeparately',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/QuoteSeparately.vue')
+        component: () => import('../views/ParameterSetting/QuoteSeparately.vue')
       },
       {
         path: '/parameterSetting/news-setting',
         name: 'parameterSetting-news-setting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/NewsSetting.vue')
+        component: () => import('../views/ParameterSetting/NewsSetting.vue')
       },
       {
         path: '/parameterSetting/document-download-setting',
         name: 'parameterSetting-document-download-setting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/ParameterSetting/DocumentDownloadSetting.vue')
+        component: () => import('../views/ParameterSetting/DocumentDownloadSetting.vue')
       }
     ]
   },
   {
     path: '/termsSetting',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //條款設定 建議條款
         path: '/termsSetting/proposedTerms',
         name: 'termsSetting-proposedTerms',
-        component: () => import(/* webpackChunkName: "about" */ '../views/TermsSetting/ProposedTerms.vue')
+        component: () => import('../views/TermsSetting/ProposedTerms.vue')
       },
       { //條款設定 另行報價額度
         path: '/termsSetting/quotationAndWeight',
         name: 'termsSetting-quotationAndWeight',
-        component: () => import(/* webpackChunkName: "about" */ '../views/TermsSetting/QuotationAndWeight.vue')
+        component: () => import('../views/TermsSetting/QuotationAndWeight.vue')
       },
       { //條款設定 另行報價和權重
         path: '/termsSetting/quotationAndAmount',
         name: 'termsSetting-quotationAndAmount',
-        component: () => import(/* webpackChunkName: "about" */ '../views/TermsSetting/QuotationAndAmount.vue')
+        component: () => import('../views/TermsSetting/QuotationAndAmount.vue')
       }
     ]
   },
   {
     path: '/underwritingCooperation',
-    component: () => import(/* webpackChunkName: "about" */ '../layouts/DefaultLayout.vue'),
+    component: () => import('../layouts/DefaultLayout.vue'),
     children: [
       { //核保明細階級設定
         path: '/underwritingCooperation/Level',
         name: 'underwritingCooperationLevel',
-        component: () => import(/* webpackChunkName: "about" */ '../views/UnderwritingCooperation/Level.vue')
+        component: () => import('../views/UnderwritingCooperation/Level.vue')
       },
       { //核保組織設定
         path: '/underwritingCooperation/Setting',
         name: 'underwritingCooperationSetting',
-        component: () => import(/* webpackChunkName: "about" */ '../views/UnderwritingCooperation/Setting.vue')
+        component: () => import('../views/UnderwritingCooperation/Setting.vue')
       }
     ]
   }
